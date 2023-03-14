5ba99bf96d1e2f8ab56b332d47ae44feadf7bc6e
Said <saidmartazanov1@mail.ru>
2023-03-14 23:22:46 +0300

    Сохранение

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const authRouter = require('./routes/authRouter')
 const dotenv = require('dotenv')
 
 dotenv.config()
-
+//сохранение
 const PORT = 5000
 const app = express()
 app.use(express.json())
