ef1eca3ec9f5ecf68aaeeca2235da2ef1e745bbc
said <saidmartazanov1@gmail.com>
2023-04-15 22:49:08 +0300

    auth

--- models/User.model.js
+++ models/User.model.js
@@ -1,9 +1,9 @@
-const {Schema,model} = require('mongoose')
+const { Schema, model } = require('mongoose')
 
 const User = new Schema({
-    username:{type:String,unique:true, required: true},
-    password:{type:String,required: true},
-    roles:[{type: String,ref:'Role'}]
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    roles: [{ type: String, ref: 'Role' }]
 })
 
-module.exports = model('User',User)
+module.exports = model('User', User)
