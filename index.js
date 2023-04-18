6a26896c32dd57d3e0e6d08649db4317726b13c2
said <saidmartazanov1@gmail.com>
2023-04-18 18:05:42 +0300

    multer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const authRouter = require('./routes/authRouter')
 const addRouter = require('./routes/upload.route')
 const dotenv = require('dotenv')
 const cors = require("cors")
+const path = require("path")
 
 
 dotenv.config()
@@ -12,6 +13,7 @@ const PORT = 5000
 const app = express()
 
 app.use(express.json())
+app.use("/images", express.static(path.join(__dirname, "images")))
 app.use(cors())
 app.use('/auth', authRouter)
 app.use('./add', addRouter)
