b6c02594ed2e5dd7256ebb84fdb6d45ad2838a52
said <saidmartazanov1@gmail.com>
2023-04-26 22:48:01 +0300

    update

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -1,8 +1,7 @@
 const { Router } = require('express')
-const express = require('express')
 const fileMiddleware = require("../middlewares/file")
 const router = Router()
-const app = express()
+
 
 const { addingController } = require('../controllers/addingController')
 
